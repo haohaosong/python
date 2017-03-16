604e543f9fefe958ff32a968510a4d0d10fd625e
haohaosong <742398269@qq.com>
2017-03-16 17:22:26 +0800

    hhs:修改文件夹名称

--- base/isAdult.py
+++
@@ -1,7 +0,0 @@
-#check a man is a teenager or an adult
-a = input('Please enter an age\n')
-a = int()
-if a>= 18:
-	print('adult')
-else:
-	print('teenager')  
--- base/print.py
+++
@@ -1,6 +0,0 @@
-#print absolute value of an interger:
-a = 100
-if a >= 0:
-	print(a)
-else:
-	print(-a)
