23858f6b11609b99c35c8788164925a4bd3d2ac1
haohaosong <742398269@qq.com>
2017-03-17 22:34:38 +0800

    hhs:练习while循环

---
+++ basic/loop.py
@@ -0,0 +1,19 @@
+#-*-coding:utf-8-*-
+
+#计算1+2+3+...+100
+sum = 0
+n = 1
+while n<=100:
+	sum+=n
+	n = n+1
+
+print(sum)
+
+#计算1*2*3*...*100
+acc = 1
+n = 1
+while n<=100:
+	acc*=n
+	n = n+1
+
+print(acc)
